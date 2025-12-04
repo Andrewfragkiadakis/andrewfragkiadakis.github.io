c28a1a207409d5699ed1df2d0bae4a9c60e54d12
Andrew Fragkiadakis <62713718+Andrewfragkiadakis@users.noreply.github.com>
2025-12-04 21:51:14 +0200

    Delete andreas-technology-v2-backup-20251204-121148 directory

--- andreas-technology-v2-backup-20251204-121148/src/hooks/useContent.ts
+++
@@ -1,7 +0,0 @@
-import { useLanguage } from '@/contexts/LanguageContext'
-import { content as allContent } from '@/data/content'
-
-export function useContent() {
-    const { language } = useLanguage()
-    return allContent[language]
-}
